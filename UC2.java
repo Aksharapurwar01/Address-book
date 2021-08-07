0168160e52ec8573ca0d6548a1c3267da0ce8a4f
Akshara Purwar <aksharapurwar@gmail.com>
2021-08-07 10:45:44 +0530

    Address book insert,delete

---
+++ UC2.java
@@ -0,0 +1,53 @@
+package mainpackage;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+import mainpackage.Print;
+import mainpackage.Add;
+import mainpackage.Search;
+import mainpackage.Change;
+import mainpackage.Delete;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.nio.charset.Charset;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		
+		Scanner input = new Scanner(System.in);
+		int exit = 0;
+		int answer;
+		//we will loop until user wants to exit the application
+		do {
+			System.out.println("--------Welcome to Address Book---------");
+			System.out.println("Enter '1' to Add contact");
+			System.out.println("Enter '2' to Edit contact");
+			System.out.println("Enter '3' to Delete contact");
+			System.out.println("Enter '0' to Exit");
+			System.out.println("Do you want to print contacts, add contact, search for contact, edit contact or delete contact?");
+			System.out.println("Answer with '1', '2', '3' or '0' to exit application.");	
+			try {//we handle the input of the user
+				answer = input.nextInt();
+			} catch (NumberFormatException e) {
+			    //e.printStackTrace();
+				answer = -1;
+			}
+			if(answer == 1)//according to user's input we go to each class
+				
+				Add.add_contact();
+			else if(answer == 2)
+				Edit.show_contacts();
+			else if(answer == 3)
+				Delete.choose_field();
+				
+			
+			
+				
+			
+		}while(answer != exit);
+		System.out.println("Application terminating...");
+	}
+}
